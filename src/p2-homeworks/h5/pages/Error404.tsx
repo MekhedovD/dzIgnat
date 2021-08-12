62cd383185fe75a3545fbfa5690eb7d7766174d2
Dmitry <mekhedov.d@mail.ru>
2021-08-12 20:47:28 +0300

    final HW5

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-// import s from "./HW5.module.css";
 import "./errorStyle.css"
 
 function Error404() {
     return (
         <div className="errorWrapper">
-            <div>404</div>
+            <div className="errText">CENSORED</div>
         </div>
     );
 }
