64778cbaed5273f3dbc20f8fcbc97f4273dfef13
Dmitry <mekhedov.d@mail.ru>
2021-08-13 19:07:29 +0300

    HW6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import SuperEditableSpan from "./common/c4-SuperEditableSpan/SuperEditableSpan";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 import {restoreState, saveState} from "./localStorage/localStorage";
+import "./styleHW6.css";
 
 function HW6() {
     const [value, setValue] = useState<string>("");
@@ -10,30 +11,34 @@ function HW6() {
         saveState<string>("editable-span-value", value);
     };
     const restore = () => {
-        // setValue();
+      const restoreValue = restoreState("editable-span-value", value);
+      setValue(restoreValue);
     };
 
     return (
-        <div>
-            <hr/>
-            homeworks 6
+      <div>
+        <hr/>
+        homeworks 6
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : "enter text..."}}
-                />
-            </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
+        {/*should work (должно работать)*/}
+        <div className="wrapperHW6">
+          <div>
+            <SuperEditableSpan
+              value={value}
+              onChangeText={setValue}
+              spanProps={{children: value ? undefined : "enter text..."}}
+            />
+          </div>
+          <SuperButton onClick={save}>save</SuperButton>
+          <SuperButton onClick={restore}>restore</SuperButton>
         </div>
+
+
+        <hr/>
+        {/*для личного творчества, могу проверить*/}
+        {/*<AlternativeSuperEditableSpan/>*/}
+        <hr/>
+      </div>
     );
 }
 
