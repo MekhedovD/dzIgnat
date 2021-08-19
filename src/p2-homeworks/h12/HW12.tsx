6c8c8a83cc5273572e4ae54d445fe452d8074047
Dmitry <mekhedov.d@mail.ru>
2021-08-19 21:30:26 +0300

    final #12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,25 +1,45 @@
 import React from "react";
 import s from "./HW12.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
 
-const themes = ['dark', 'red', 'some'];
+const themes: Array<string> = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+  const theme = useSelector((state: AppStoreType) => state.theme.themes); // useSelector
+  const dispatch = useDispatch();
+  const onChangeCallback = (option: string): void => {
+    if (option === "dark") {
+      dispatch({type: "SET-DARK-THEME"})
+    }
+    if (option === "red") {
+      dispatch({type: "SET-RED-THEME"})
+    }
+    if (option === "some") {
+      dispatch({type: "SET-SOME-THEME"})
+    }
+  }
 
-    // useDispatch, onChangeCallback
+  // useDispatch, onChangeCallback
 
-    return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
+  return (
+    <div className={s[theme]}>
+      <hr/>
+      <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
+      <div className={s.themeHW12}>
+        <SuperSelect
+          options={themes}
+          onChangeOption={onChangeCallback}
+        />
+      </div>
+      {/*should work (должно работать)*/}
+      {/*SuperSelect or SuperRadio*/}
 
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
-
-            <hr/>
-        </div>
+      <hr/>
+    </div>
     );
 }
 
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,11 +1,35 @@
-const initState = {
+export type ThemesInitState = {
+    themes: string
+}
 
+type SetDarkThemeAC = {
+    type: "SET-DARK-THEME"
+}
+
+type SetRedThemeAC = {
+    type: "SET-RED-THEME"
+}
+
+type SetSomeThemeAC = {
+    type: "SET-SOME-THEME"
+}
+
+export type ThemeACType = SetDarkThemeAC | SetRedThemeAC | SetSomeThemeAC;
+
+const initState = {
+    themes: "some"
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state:ThemesInitState = initState, action: ThemeACType): ThemesInitState => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "SET-DARK-THEME": {
+            return {themes: "dark"};
+        }
+        case "SET-RED-THEME": {
+            return {themes: "red"}
+        }
+        case "SET-SOME-THEME": {
+            return {themes: "some"}
         }
         default: return state;
     }
